4d76a70a747a81a8b9e03c7c1d1ff34d8d8468a7
hlebec-tukallec <67371825+hlebec-tukallec@users.noreply.github.com>
2021-02-26 13:34:11 +0300

    Add files via upload

--- Dichotomy.java
+++ Dichotomy.java
@@ -1,16 +1,20 @@
 public class Dichotomy implements Method {
     private double res;
+    double delta = EPS / 4;
 
     private void count() {
-        double a, b, c;
+        double a, b, x1, x2;
         a = A;
         b = B;
         while (Math.abs(b - a) > EPS) {
-            c = (a + b) / 2;
-            if (Method.f(b) * Method.f(c) < 0) {
-                a = c;
+
+            x1 = (a + b) / 2 - delta;
+            x2 = (a + b) / 2 + delta;
+
+            if (Method.f(x1) > Method.f(x2)) {
+                a = x1;
             } else {
-                b = c;
+                b = x2;
             }
         }
         res = (a + b) / 2;
--- Main.java
+++ Main.java
@@ -8,4 +8,6 @@ public class Main {
     }
     //6. f(x) = −5x^5 + 4x^4 − 12x^3 + 11x^2 − 2x + 1 → min на интервале [-0.5; 0.5]
 
+
+    //wolfram says: 0.10986
 }
