d5b1c0a8f1232e6fd9072f58a69f1036244bafdc
milkymay <missisPatrik@yandex.ru>
2021-04-12 20:46:02 +0300

    lab2 shlakkk deleted

--- lab2/shlakkk/GradDown.java
+++
@@ -1,6 +0,0 @@
-public class GradDown extends CommonDown {
-    public GradDown() {
-        super((args) -> args.x.x / 2);
-    }
-
-}
--- lab2/shlakkk/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Method gradDown = new GradDown();
-        gradDown.count();
-        Method fastDown = new FastDown("");
-       // fastDown.count();
-    }
-}
