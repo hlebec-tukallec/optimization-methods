5bf9eaa99eeee9704317a0c4fb81ee3360375e8a
milkymay <missisPatrik@yandex.ru>
2021-06-03 23:49:56 +0300

    Main

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
     5: 3x^2+xy+2y^2-x-4y (1.1.1)
     6: -y*sqrt(x)+2*y^2+x-14y (1.1.2)
 */
-    private static int mod = 5;
+    private static int mod = 0;
 /*
     0: Newton();
     1: NewtonDirectionDescent();
@@ -31,31 +31,44 @@ public class Main {
         Source source = new Source();
         source.changeMod(mod);
 
-        Method method;
-        switch (methodMod) {
-            case (0): {
-                method = new Newton();
-            }
-            case (1): {
-                method = new NewtonDirectionDescent();
-            }
-            case (2): {
-                method = new NewtonOptimisation();
-            }
-            case (3): {
-                method = new BroadenFletcherChen();
-            }
-            case (4): {
-                method = new Powell();
-            }
-            default: {
-                method = new Newton();
-            }
+        for (int i = 0; i < 5; i++) {
+            methodMod = i;
+            Method method;
+            switch (methodMod) {
+                case (0): {
+                    method = new Newton();
+                    System.out.println("Newton");
+                    break;
+                }
+                case (1): {
+                    method = new NewtonDirectionDescent();
+                    System.out.println("NewtonDirectionDescent");
+                    break;
+                }
+                case (2): {
+                    method = new NewtonOptimisation();
+                    System.out.println("NewtonOptimisation");
+                    break;
+                }
+                case (3): {
+                    method = new BroadenFletcherChen();
+                    System.out.println("NewtonFletcherChen");
+                    break;
+                }
+                case (4): {
+                    method = new Powell();
+                    System.out.println("Powell");
+                    break;
+                }
+                default: {
+                    method = new Newton();
+                }
 
+            }
+            Point res = method.minimum(source.getFunction(), source.getPoint(), 0.000001);
+            System.out.println("ans" + res);
+            System.out.println(method.getCountOfIterations());
+            System.out.println();
         }
-        Point res = method.minimum(source.getFunction(), source.getPoint(), 0.000001);
-        System.out.println("ans" + res);
-        System.out.println(method.getCountOfIterations());
-        System.out.println();
     }
 }
