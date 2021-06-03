0de81f6f4e098b8ec823ee20817e5328c6f8a71b
milkymay <missisPatrik@yandex.ru>
2021-06-03 23:38:26 +0300

    Main with methodMod

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -8,36 +8,54 @@ import utils.Point;
 import utils.Source;
 
 public class Main {
+/*
+    0: 1.2.1
+    1: 1.2.2 / 2.1
+    2: 2.2
+    3: 2.3
+    4: 2.4
+    5: 3x^2+xy+2y^2-x-4y (1.1.1)
+    6: -y*sqrt(x)+2*y^2+x-14y (1.1.2)
+*/
     private static int mod = 5;
+/*
+    0: Newton();
+    1: NewtonDirectionDescent();
+    2: NewtonOptimisation();
+    3: BroadenFletcherChen();
+    4: Powell();
+ */
+    private static int methodMod = 0;
 
     public static void main(String[] args) {
         Source source = new Source();
         source.changeMod(mod);
 
-        /*Method newton = new Newton();
-        Point res = newton.minimum(source.getFunction(), source.getPoint(), 0.000001);
-        System.out.println("ans" + res);
-        System.out.println(newton.getCountOfIterations());
-        System.out.println();*/
+        Method method;
+        switch (methodMod) {
+            case (0): {
+                method = new Newton();
+            }
+            case (1): {
+                method = new NewtonDirectionDescent();
+            }
+            case (2): {
+                method = new NewtonOptimisation();
+            }
+            case (3): {
+                method = new BroadenFletcherChen();
+            }
+            case (4): {
+                method = new Powell();
+            }
+            default: {
+                method = new Newton();
+            }
 
-        Method newtonDirect = new NewtonDirectionDescent();
-        Point res = newtonDirect.minimum(source.getFunction(), source.getPoint(), 0.000001);
+        }
+        Point res = method.minimum(source.getFunction(), source.getPoint(), 0.000001);
         System.out.println("ans" + res);
-        System.out.println(newtonDirect.getCountOfIterations());
+        System.out.println(method.getCountOfIterations());
         System.out.println();
-
-      /*  Method newtonOptimisation = new NewtonOptimisation();
-        res = newtonOptimisation.minimum(source.getFunction(), source.getPoint(), 0.000001);
-        System.out.println("ans" + res);
-        System.out.println(newtonOptimisation.getCountOfIterations());
-*/
-        /*res = new NewtonDirectionDescent().minimum(source.getFunction(), source.getPoint(), 0.0001);
-        System.out.println(res); // ok
-        res = new NewtonOptimisation().minimum(source.getFunction(), source.getPoint(), 0.0001);
-        System.out.println(res); // ok
-        res = new BroadenFletcherChen().minimum(source.getFunction(), source.getPoint(), 0.0001);
-        System.out.println(res); //ok
-        res = new Powell().minimum(source.getFunction(), source.getPoint(), 0.0001);
-        System.out.println(res); //ok*/
     }
 }
